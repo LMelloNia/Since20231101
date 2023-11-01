463e1ca7176371618fcf231b995d421c59e16e88
LMelloNia <ch10152080@gmail.com>
2023-11-01 17:10:11 +0900

    프로젝트 생성

---
+++ Since20231101/Since20231101/Since20231101App.swift
@@ -0,0 +1,17 @@
+//
+//  Since20231101App.swift
+//  Since20231101
+//
+//  Created by 김시훈 on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Since20231101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
